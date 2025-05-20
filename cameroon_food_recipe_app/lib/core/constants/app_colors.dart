faa7b3c012a68811f9d30f1faf688ebc3ef6fb99
gghislain92 <ghislainche20@gmail.com>
2025-05-20 19:19:45 +0300

    Bugs fixed in app_theme and signup screen

---
+++ cameroon_food_recipe_app/lib/core/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFE67E22); // Orange color from the mockup
+  static const Color secondary = Color(0xFFFFA500);
+  static const Color background = Colors.white;
+  static const Color text = Color(0xFF333333);
+  static const Color textLight = Color(0xFF757575);
+  static const Color inputBackground = Color(0xFFF5F5F5);
+  static const Color inputBorder = Color(0xFFE0E0E0);
+}
